095e327d3c6f81f90edb252e02150b09c425382e
Guilherme <gloureiro@mytechretail.com>
2022-02-22 17:26:35 +0100

    first commit

--- server.js
+++ server.js
@@ -1,11 +1,9 @@
-const path = require('path');
 const express = require('express');
+const path = require('path');
 const app = express();
-
-// Serve static files
 app.use(express.static(__dirname + '/dist/helloteca-test'));
-
-// Send all requests to index.html
-app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/helloteca-test/index.html'));
+app.get('/*', function (req, res) {
+    res.sendFile(path.join(__dirname +
+        '/dist/helloteca-test/index.html'));
 });
+app.listen(process.env.PORT || 8080);
